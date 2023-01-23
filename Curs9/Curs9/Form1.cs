62e4cbf3e19efec157135ab6f057f2d80f471b0d
georgerat <ratgeorgeraul2018@gmail.com>
2023-01-23 15:44:28 +0200

    add Curs9 - Roy-Warshall, componente tare conexe

---
+++ Curs9/Curs9/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Curs9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graph g = new Graph();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g.load(@"../../TextFile1.txt");
+            List<string> list = g.view(g.matrix);
+            foreach (string s in list)
+                listBox1.Items.Add(s);
+            listBox1.Items.Add('\n');
+            g.Roy_Warshall();
+            List<string> list2 = g.view(g.mdrumuri);
+            foreach (string s in list2)
+                listBox1.Items.Add(s);
+            listBox1.Items.Add('\n');
+            List<string> list3 = g.CTC();
+            foreach (string s in list3)
+                listBox1.Items.Add(s);
+        }
+    }
+}
---
+++ Curs9/Curs9/Graph.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace Curs9
+{
+    internal class Graph
+    {
+        public int n;
+        public int[,] matrix;
+        public int[,] mdrumuri;
+
+        public void load(string fileName)
+        {
+            TextReader reader = new StreamReader(fileName);
+            n = int.Parse(reader.ReadLine());
+
+            matrix = new int[n + 1, n + 1];
+            mdrumuri = new int[n + 1, n + 1];
+
+            string buffer;
+            while ((buffer = reader.ReadLine()) != null)
+            {
+                string[] t = buffer.Split(' ');
+                int x = int.Parse(t[0]);
+                int y = int.Parse(t[1]);
+                matrix[x, y] = 1;
+            }
+        }
+
+        public List<string> view(int[,] m)
+        {
+            List<string> list = new List<string>();
+            string buffer;
+            for (int i = 1; i <= n; i++)
+            {
+                buffer = "";
+                for (int j = 1; j <= n; j++)
+                {
+                    buffer += m[i, j];
+                }
+                list.Add(buffer);
+            }
+            return list;
+        }
+
+        public void Roy_Warshall()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                    mdrumuri[i, j] = matrix[i, j];
+            }
+
+            for (int k = 1; k <= n; k++)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (mdrumuri[i, j] == 0)
+                                mdrumuri[i, j] = mdrumuri[i, k] * mdrumuri[k, j];
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<string> CTC()
+        {
+            List<string> ctc = new List<string>();
+            int[] p = new int[n + 1];
+            string buffer;
+
+            for (int i = 1; i <= n; i++)
+                p[i] = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                buffer = "";
+                if (p[i] == 0)
+                {
+                    buffer += i + " ";
+                    p[i] = 1;
+                    for (int j = 1; j <= n; j++)
+                        if (mdrumuri[i, j] * mdrumuri[j, i] == 1)
+                        {
+                            buffer += j + " ";
+                            p[j] = 1;
+                        }
+                }
+                if (buffer != "")
+                    ctc.Add(buffer);
+            }
+            return ctc;
+        }
+    }
+}
