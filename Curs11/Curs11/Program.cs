2c443c2c851217a5b3dab22e11effb7a578e5a06
georgerat <ratgeorgeraul2018@gmail.com>
2023-01-24 15:55:46 +0200

    add Curs11 - algoritm genetic

---
+++ Curs11/Curs11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Curs11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Engine.LoadFromFile(@"../../TextFile1.txt");
+            AlgGenetic demo = new AlgGenetic();
+            demo.InitPop();
+            for (int i = 0; i < 1000000; i++) //alegem timpul de evolutie un milion
+            {
+                demo.SortPop();
+                demo.SelectPop();
+                demo.UpdatePop();
+            }
+            demo.SortPop(); //dupa aceasta sortare, prinmul individ e solutia noastra
+            demo.View();
+        }
+    }
+}
---
+++ Curs11/Curs11/Sol.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Curs11
+{
+    public class Sol
+    {
+        public int[] v;
+
+        public Sol() //initializeaza valorile initiale, solutiile initilae
+        {
+            v = new int[Engine.n];
+            for (int i = 0; i < Engine.n; i++)
+                v[i] = Engine.rnd.Next(Engine.min, Engine.max);
+        }
+
+        public Sol(Sol toClone) //constructor de copiere
+        {
+            v = new int[Engine.n];
+            for (int i = 0; i < Engine.n; i++)
+                this.v[i] = toClone.v[i];
+        }
+
+        public float fadec()
+        {
+            float sumaGLobala = 0;
+            for (int i = 0; i < Engine.n; i++)
+            {
+                float sumaLocala = 0;
+                for (int j = 0; j < Engine.n; j++)
+                    sumaLocala = Engine.A[i, j] * v[j];
+                sumaLocala -= Engine.T[i];
+                sumaGLobala += Math.Abs(sumaLocala);
+            }
+            return sumaGLobala;
+        }
+
+        public string View()
+        {
+            string toR = "";
+            for (int i = 0; i < Engine.n; i++)
+                toR += v[i] + " ";
+            return toR;
+        }
+    }
+}
